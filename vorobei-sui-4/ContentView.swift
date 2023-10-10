cdbff94edb568fd18051b80af5944768fb3446a9
Alexey Voronov <voronoff2803@gmail.com>
2023-10-10 19:12:51 +0300

    Initial Commit

---
+++ vorobei-sui-4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  vorobei-sui-4
+//
+//  Created by Alexey Voronov on 10.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ vorobei-sui-4/vorobei_sui_4App.swift
@@ -0,0 +1,17 @@
+//
+//  vorobei_sui_4App.swift
+//  vorobei-sui-4
+//
+//  Created by Alexey Voronov on 10.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct vorobei_sui_4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
